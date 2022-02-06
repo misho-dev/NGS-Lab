879496f9a405d70849b4fd227142c664cb04ad52
dkipiani <dimitri.kipiani@scandiweb.com>
2022-02-06 15:32:47 +0400

    Added meta thumbnails to dynamic pages

--- App/Controller/Blog/Page.php
+++ App/Controller/Blog/Page.php
@@ -3,7 +3,9 @@
 namespace App\Controller\Blog;
 
 use App\Controller\ControllerAction;
+use App\Model\Image;
 use App\Model\Repository\Blog;
+use App\Model\Repository\Image as ImageRepository;
 use App\ViewModel\View;
 
 class Page implements ControllerAction
@@ -16,8 +18,9 @@ class Page implements ControllerAction
         if (!$blog) {
             View::render('contact.phtml'); // TODO: 404 page
         } else {
-            View::render('blog-page.phtml', compact('blog'));
-        }
+            $image = ImageRepository::getImageById($blog->getImage()) ?? new Image();
 
+            View::render('blog-page.phtml', compact('image'));
+        }
     }
 }
