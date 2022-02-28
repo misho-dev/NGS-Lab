0be91a40934f9e6e8c1100cf76d934d5bc8e26d6
misho-dev <msaam19@freeuni.edu.ge>
2022-02-28 19:26:58 +0400

    bug fixes

--- static/js/card-controler.js
+++ static/js/card-controler.js
@@ -1,15 +1,15 @@
-const ownerCards = document.querySelectorAll(".product-owner-card");
+    const ownerCards = document.querySelectorAll(".product-owner-card");
 
-ownerCards.forEach(card => {
-    card.addEventListener("mouseover", e =>{
-        if(e.target.classList.contains('default-card')  ){
-            e.target.style.opacity = "0";
-            setTimeout(() => {e.target.style.visibility = "hidden"}, 300);
-        }
+    ownerCards.forEach(card => {
+        card.addEventListener("mouseover", e =>{
+            if(e.target.classList.contains('default-card')  ){
+                e.target.style.pointerEvents = "none";
+                e.target.style.opacity = "0";
+            }
+        })
+        card.addEventListener("mouseleave", e=>{
+            let defaultCard = e.target.childNodes[1].childNodes[1].childNodes[1];
+            defaultCard.style.pointerEvents = "auto";
+            defaultCard.style.opacity = "1";
+        })
     })
-    card.addEventListener("mouseleave", e=>{
-        let defaultCard = e.target.childNodes[1].childNodes[1].childNodes[1];
-        defaultCard.style.opacity = "1";
-        defaultCard.style.visibility = "visible"
-    })
-})
